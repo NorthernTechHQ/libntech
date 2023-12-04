00815bd2c199fd2377d482ae2cea7c3132b5ac31
Vratislav Podzimek <vratislav.podzimek@northern.tech>
2023-12-04 12:31:53 +0100

    Add unit tests for StringMatchCaptures()
    Ticket: ENT-10629
    Changelog: None

--- tests/unit/regex_test.c
+++ tests/unit/regex_test.c
@@ -6,6 +6,8 @@
 #error "PCRE2 required for regex tests"
 #endif
 
+#include <buffer.h>
+
 #include <test.h>
 
 static void test_match(void)
@@ -26,6 +28,42 @@ static void test_match_full(void)
 }
 
 
+static void test_match_with_captures(void)
+{
+    Seq *ret = StringMatchCaptures("^a(.).*$", "abc", false);
+    assert_true(ret);
+    assert_int_equal(SeqLength(ret), 2);
+    assert_string_equal(BufferData(SeqAt(ret, 0)), "abc");
+    assert_string_equal(BufferData(SeqAt(ret, 1)), "b");
+    SeqDestroy(ret);
+
+    ret = StringMatchCaptures("^a(.).*$", "abc", true);
+    assert_true(ret);
+    assert_int_equal(SeqLength(ret), 4);
+    assert_string_equal(BufferData(SeqAt(ret, 0)), "0");
+    assert_string_equal(BufferData(SeqAt(ret, 1)), "abc");
+    assert_string_equal(BufferData(SeqAt(ret, 2)), "1");
+    assert_string_equal(BufferData(SeqAt(ret, 3)), "b");
+    SeqDestroy(ret);
+
+    ret = StringMatchCaptures("^a(?<mid>..)d*$", "abcd", false);
+    assert_true(ret);
+    assert_int_equal(SeqLength(ret), 2);
+    assert_string_equal(BufferData(SeqAt(ret, 0)), "abcd");
+    assert_string_equal(BufferData(SeqAt(ret, 1)), "bc");
+    SeqDestroy(ret);
+
+    ret = StringMatchCaptures("^a(?<mid>..)d*$", "abcd", true);
+    assert_true(ret);
+    assert_int_equal(SeqLength(ret), 4);
+    assert_string_equal(BufferData(SeqAt(ret, 0)), "0");
+    assert_string_equal(BufferData(SeqAt(ret, 1)), "abcd");
+    assert_string_equal(BufferData(SeqAt(ret, 2)), "mid");
+    assert_string_equal(BufferData(SeqAt(ret, 3)), "bc");
+    SeqDestroy(ret);
+}
+
+
 int main()
 {
     PRINT_TEST_BANNER();
@@ -33,6 +71,7 @@ int main()
     {
         unit_test(test_match),
         unit_test(test_match_full),
+        unit_test(test_match_with_captures),
     };
 
     return run_tests(tests);
