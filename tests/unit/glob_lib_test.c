2d666d84ffc89ea63468627044641f0caa456727
Lars Erik Wik <lars.erik.wik@northern.tech>
2024-01-26 15:36:35 +0100

    Added rigorous testing of GlobFileList()
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- tests/unit/glob_lib_test.c
+++ tests/unit/glob_lib_test.c
@@ -259,10 +259,8 @@ static void test_glob_match(void)
 
 static void test_glob_find(void)
 {
-    /* This test is not very thorough, because we don't want unit tests to
-     * create or depend on too many files. More testing will be done through
-     * acceptance tests.
-     */
+    /* This test is not very thorough. However, test_glob_file_list()
+     * indirectly tests this one. */
     {
         Seq *const matches = GlobFind("test.{h,c}");
         assert_int_equal(SeqLength(matches), 2);
@@ -278,22 +276,491 @@ static void test_glob_find(void)
     }
 }
 
+#endif // WITH_PCRE2
 
 static void test_glob_file_list(void)
 {
-    /* This test is not very thorough, because we don't want unit tests to
-     * create or depend on too many files. More testing will be done through
-     * acceptance tests.
+    // Create temporary directory.
+
+    char template[] = "test_glob_file_list_XXXXXX";
+    const char *const test_dir = mkdtemp(template);
+    assert_true(test_dir != NULL);
+
+    // Create test subdirectories.
+
+    static const char *const test_subdirs[] = {
+        "foo",
+        "foo" FILE_SEPARATOR_STR "bar",
+        "foo" FILE_SEPARATOR_STR "bar" FILE_SEPARATOR_STR "baz",
+        "qux",
+    };
+    const size_t num_test_subdirs =
+        sizeof(test_subdirs) / sizeof(const char *);
+
+    char path[PATH_MAX];
+    for (size_t i = 0; i < num_test_subdirs; i++)
+    {
+        int ret = snprintf(
+            path,
+            PATH_MAX,
+            "%s" FILE_SEPARATOR_STR "%s",
+            test_dir,
+            test_subdirs[i]);
+        assert_false(ret >= PATH_MAX || ret < 0);
+        ret = mkdir(path, (mode_t) 0700);
+        assert_int_equal(ret, 0);
+    }
+
+    // Create test files.
+
+    static const char *const test_files[] = {
+#ifdef WITH_PCRE2
+        /* POSIX glob(3) does not support GLOB_PERIOD, meaning it will not find
+         * this file, thus cause tests below to fail. */
+        ".gitignore",
+#endif // WITH_PCRE2
+        "README.md",
+        "foo" FILE_SEPARATOR_STR "README.md",
+        "foo" FILE_SEPARATOR_STR "bank_statements.txt",
+        "foo" FILE_SEPARATOR_STR "bar" FILE_SEPARATOR_STR "README.md",
+        "foo" FILE_SEPARATOR_STR "bar" FILE_SEPARATOR_STR "passwords.txt",
+        "foo" FILE_SEPARATOR_STR "bar" FILE_SEPARATOR_STR "secret.gpg",
+        "foo" FILE_SEPARATOR_STR "bar" FILE_SEPARATOR_STR
+        "baz" FILE_SEPARATOR_STR "README.md",
+        "foo" FILE_SEPARATOR_STR "bar" FILE_SEPARATOR_STR
+        "baz" FILE_SEPARATOR_STR "const is love",
+        "foo" FILE_SEPARATOR_STR "bar" FILE_SEPARATOR_STR
+        "baz" FILE_SEPARATOR_STR "const is life",
+        "qux" FILE_SEPARATOR_STR "README.md",
+        "qux" FILE_SEPARATOR_STR "source.c",
+        "qux" FILE_SEPARATOR_STR "source.h",
+        "qux" FILE_SEPARATOR_STR "source.o",
+        "qux" FILE_SEPARATOR_STR "source.cpp",
+        "qux" FILE_SEPARATOR_STR "source.hpp",
+    };
+    const size_t num_test_files = sizeof(test_files) / sizeof(const char *);
+
+    for (size_t i = 0; i < num_test_files; i++)
+    {
+        const int ret = snprintf(
+            path,
+            PATH_MAX,
+            "%s" FILE_SEPARATOR_STR "%s",
+            test_dir,
+            test_files[i]);
+        assert_true(ret < PATH_MAX && ret >= 0);
+
+        const int fd = open(path, O_CREAT, (mode_t) 0600);
+        assert_int_not_equal(fd, -1);
+        close(fd);
+    }
+
+    /* Let's do some rigorous testing! The file system should look something
+     * like this:
+     *
+     *    test_glob_file_list_XXXXXX/
+     *    +-- .gitignore
+     *    +-- README.md
+     *    +-- foo/
+     *    |   +-- README.md
+     *    |   +-- bank_statements.txt
+     *    |   +-- bar/
+     *    |       +-- README.md
+     *    |       +-- passwords.txt
+     *    |       +-- secret.gpg
+     *    |       +-- baz/
+     *    |           +-- README.md
+     *    |           +-- "const is love"
+     *    |           +-- "const is life"
+     *    +-- qux/
+     *        +-- README.md
+     *        +-- source.c
+     *        +-- source.h
+     *        +-- source.o
+     *        +-- source.cpp
+     *        +-- source.hpp
      */
-    StringSet *const matches = GlobFileList("test.{h,c}");
+
+    //////////////////////////////////////////////////////////////////////////
+    // Check that we can find all files with a relative path glob pattern.
+    //////////////////////////////////////////////////////////////////////////
+
+    char pattern[PATH_MAX];
+    int ret = snprintf(pattern, PATH_MAX, "%s/**/*", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+
+    StringSet *matches = GlobFileList(pattern);
+    assert_int_equal(
+        StringSetSize(matches), num_test_files + num_test_subdirs);
+
+    for (size_t i = 0; i < num_test_files; i++)
+    {
+        const int ret = snprintf(
+            path,
+            PATH_MAX,
+            "%s" FILE_SEPARATOR_STR "%s",
+            test_dir,
+            test_files[i]);
+        assert_true(ret < PATH_MAX && ret >= 0);
+        assert_true(StringSetContains(matches, path));
+    }
+
+    for (size_t i = 0; i < num_test_subdirs; i++)
+    {
+        const int ret = snprintf(
+            path,
+            PATH_MAX,
+            "%s" FILE_SEPARATOR_STR "%s",
+            test_dir,
+            test_subdirs[i]);
+        assert_true(ret < PATH_MAX && ret >= 0);
+        assert_true(StringSetContains(matches, path));
+    }
+
+    StringSetDestroy(matches);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Check that we can find all files with an absolute path glob pattern.
+    //////////////////////////////////////////////////////////////////////////
+
+    char cwd[PATH_MAX];
+    assert_true(getcwd(cwd, PATH_MAX) != NULL);
+
+    ret = snprintf(pattern, PATH_MAX, "%s/%s/**/*", cwd, test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+
+    matches = GlobFileList(pattern);
+    assert_int_equal(
+        StringSetSize(matches), num_test_files + num_test_subdirs);
+
+    for (size_t i = 0; i < num_test_files; i++)
+    {
+        const int ret = snprintf(
+            path,
+            PATH_MAX,
+            "%s" FILE_SEPARATOR_STR "%s" FILE_SEPARATOR_STR "%s",
+            cwd,
+            test_dir,
+            test_files[i]);
+        assert_true(ret < PATH_MAX && ret >= 0);
+        assert_true(StringSetContains(matches, path));
+    }
+
+    for (size_t i = 0; i < num_test_subdirs; i++)
+    {
+        const int ret = snprintf(
+            path,
+            PATH_MAX,
+            "%s" FILE_SEPARATOR_STR "%s" FILE_SEPARATOR_STR "%s",
+            cwd,
+            test_dir,
+            test_subdirs[i]);
+        assert_true(ret < PATH_MAX && ret >= 0);
+        assert_true(StringSetContains(matches, path));
+    }
+
+    StringSetDestroy(matches);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Let's find 'foo', 'bar', 'baz', 'qux' using brace expansion
+    //////////////////////////////////////////////////////////////////////////
+
+#ifdef WITH_PCRE2 // POSIX glob(3) does not support glob expansion.
+    ret = snprintf(pattern, PATH_MAX, "%s/**/{foo,bar,baz,qux}", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    matches = GlobFileList(pattern);
+    assert_int_equal(StringSetSize(matches), 4);
+
+    ret = snprintf(path, PATH_MAX, "%s" FILE_SEPARATOR_STR "foo", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "foo" FILE_SEPARATOR_STR "bar",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "foo" FILE_SEPARATOR_STR
+        "bar" FILE_SEPARATOR_STR "baz",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(path, PATH_MAX, "%s" FILE_SEPARATOR_STR "qux", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    StringSetDestroy(matches);
+#endif // WITH_PCRE2
+
+    //////////////////////////////////////////////////////////////////////////
+    // Let's find all markdown files
+    //////////////////////////////////////////////////////////////////////////
+
+    ret = snprintf(pattern, PATH_MAX, "%s/**/*.md", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    matches = GlobFileList(pattern);
+
+    assert_int_equal(StringSetSize(matches), 5);
+
+    ret = snprintf(
+        path, PATH_MAX, "%s" FILE_SEPARATOR_STR "README.md", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "foo" FILE_SEPARATOR_STR "README.md",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "foo" FILE_SEPARATOR_STR
+        "bar" FILE_SEPARATOR_STR "README.md",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "foo" FILE_SEPARATOR_STR
+        "bar" FILE_SEPARATOR_STR "baz" FILE_SEPARATOR_STR "README.md",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "qux" FILE_SEPARATOR_STR "README.md",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    StringSetDestroy(matches);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Let's find all one letter extension files
+    //////////////////////////////////////////////////////////////////////////
+
+    ret = snprintf(pattern, PATH_MAX, "%s/**/*.?", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    matches = GlobFileList(pattern);
+
+    assert_int_equal(StringSetSize(matches), 3);
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "qux" FILE_SEPARATOR_STR "source.c",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "qux" FILE_SEPARATOR_STR "source.h",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "qux" FILE_SEPARATOR_STR "source.o",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    StringSetDestroy(matches);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Let's find all C++ files
+    //////////////////////////////////////////////////////////////////////////
+
+    ret = snprintf(pattern, PATH_MAX, "%s/qux/source.[ch]pp", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    matches = GlobFileList(pattern);
+
     assert_int_equal(StringSetSize(matches), 2);
-    assert_true(StringSetContains(matches, "test.c"));
-    assert_true(StringSetContains(matches, "test.h"));
-    assert_false(StringSetContains(matches, "test.o"));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "qux" FILE_SEPARATOR_STR "source.cpp",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "qux" FILE_SEPARATOR_STR "source.hpp",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    StringSetDestroy(matches);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Let's find const is {love,life} files
+    //////////////////////////////////////////////////////////////////////////
+
+    ret = snprintf(
+        pattern, PATH_MAX, "%s/foo/bar/baz/const is l[oi][vf]e", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    matches = GlobFileList(pattern);
+
+    assert_int_equal(StringSetSize(matches), 2);
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "foo" FILE_SEPARATOR_STR
+        "bar" FILE_SEPARATOR_STR "baz" FILE_SEPARATOR_STR "const is love",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "foo" FILE_SEPARATOR_STR
+        "bar" FILE_SEPARATOR_STR "baz" FILE_SEPARATOR_STR "const is life",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    StringSetDestroy(matches);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Let's find dot files
+    //////////////////////////////////////////////////////////////////////////
+
+#ifdef WITH_PCRE2
+    /* POSIX glob(3) does not support GLOB_PERIOD, meaning it will not find
+     * this file, thus cause tests below to fail. */
+    ret = snprintf(pattern, PATH_MAX, "%s/**/.*", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    matches = GlobFileList(pattern);
+
+    assert_int_equal(StringSetSize(matches), 1);
+
+    ret = snprintf(
+        path, PATH_MAX, "%s" FILE_SEPARATOR_STR ".gitignore", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    StringSetDestroy(matches);
+#endif // WITH_PCRE2
+
+    //////////////////////////////////////////////////////////////////////////
+    // Let's use ranges
+    //////////////////////////////////////////////////////////////////////////
+
+    ret = snprintf(pattern, PATH_MAX, "%s/**/[b-f][a-o][o-y]", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    matches = GlobFileList(pattern);
+
+    assert_int_equal(StringSetSize(matches), 2);
+
+    ret = snprintf(path, PATH_MAX, "%s" FILE_SEPARATOR_STR "foo", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "foo" FILE_SEPARATOR_STR "bar",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    StringSetDestroy(matches);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Let's find exact match
+    //////////////////////////////////////////////////////////////////////////
+
+    ret = snprintf(pattern, PATH_MAX, "%s/foo/bank_statements.txt", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    matches = GlobFileList(pattern);
+
+    assert_int_equal(StringSetSize(matches), 1);
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "foo" FILE_SEPARATOR_STR "bank_statements.txt",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    StringSetDestroy(matches);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Let's combine a whole bunch of different wildcards
+    //////////////////////////////////////////////////////////////////////////
+
+#ifdef WITH_PCRE2 // POSIX glob(3) does not support brace expansion.
+    ret = snprintf(
+        pattern,
+        PATH_MAX,
+        "%s/{foo,bar,baz}/[bf][!b-z]{r,z}/**/*c[r]e?.?p?",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    matches = GlobFileList(pattern);
+
+    assert_int_equal(StringSetSize(matches), 1);
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "foo" FILE_SEPARATOR_STR
+        "bar" FILE_SEPARATOR_STR "secret.gpg",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
+    StringSetDestroy(matches);
+#endif // WITH_PCRE2
+
+    //////////////////////////////////////////////////////////////////////////
+    // Let's use the '.' and '..' directory entries in the pattern.
+    //////////////////////////////////////////////////////////////////////////
+
+    ret = snprintf(
+        pattern, PATH_MAX, "%s/foo/../qux/../qux/./source.o", test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    matches = GlobFileList(pattern);
+
+    assert_int_equal(StringSetSize(matches), 1);
+
+    ret = snprintf(
+        path,
+        PATH_MAX,
+        "%s" FILE_SEPARATOR_STR "foo" FILE_SEPARATOR_STR
+        ".." FILE_SEPARATOR_STR "qux" FILE_SEPARATOR_STR
+        ".." FILE_SEPARATOR_STR "qux" FILE_SEPARATOR_STR "." FILE_SEPARATOR_STR
+        "source.o",
+        test_dir);
+    assert_true(ret < PATH_MAX && ret >= 0);
+    assert_true(StringSetContains(matches, path));
+
     StringSetDestroy(matches);
 }
-
-#endif // WITH_PCRE2
 
 int main()
 {
@@ -306,8 +773,8 @@ int main()
         unit_test(test_translate_glob),
         unit_test(test_glob_match),
         unit_test(test_glob_find),
-        unit_test(test_glob_file_list),
 #endif // WITH_PCRE2
+        unit_test(test_glob_file_list),
     };
 
     return run_tests(tests);
