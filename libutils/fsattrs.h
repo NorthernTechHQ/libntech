6251105dde3bbdd1750c6a2afa64645fefc65625
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-04-01 11:16:21 +0200

    Formatted fsattrs.h with clang-format
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- libutils/fsattrs.h
+++ libutils/fsattrs.h
@@ -27,11 +27,12 @@
 
 #include <stdbool.h>
 
-typedef enum {
-    FS_ATTRS_SUCCESS = 0,     /* Operation succeeded */
-    FS_ATTRS_FAILURE,         /* Unexpected failure */
-    FS_ATTRS_DOES_NOT_EXIST,  /* File does not exists */
-    FS_ATTRS_NOT_SUPPORTED,   /* Operation is not supported */
+typedef enum
+{
+    FS_ATTRS_SUCCESS = 0,    /* Operation succeeded */
+    FS_ATTRS_FAILURE,        /* Unexpected failure */
+    FS_ATTRS_DOES_NOT_EXIST, /* File does not exists */
+    FS_ATTRS_NOT_SUPPORTED,  /* Operation is not supported */
 } FSAttrsResult;
 
 /**
@@ -41,17 +42,17 @@ typedef enum {
  */
 static inline const char *FSAttrsErrorCodeToString(FSAttrsResult res)
 {
-  switch (res)
-  {
-  case FS_ATTRS_SUCCESS:
-    return "Success";
-  case FS_ATTRS_FAILURE:
-    return "Unexpected failure";
-  case FS_ATTRS_DOES_NOT_EXIST:
-    return "File does not exist";
-  case FS_ATTRS_NOT_SUPPORTED:
-    return "Operation is not supported";
-  }
+    switch (res)
+    {
+    case FS_ATTRS_SUCCESS:
+        return "Success";
+    case FS_ATTRS_FAILURE:
+        return "Unexpected failure";
+    case FS_ATTRS_DOES_NOT_EXIST:
+        return "File does not exist";
+    case FS_ATTRS_NOT_SUPPORTED:
+        return "Operation is not supported";
+    }
 }
 
 /**
